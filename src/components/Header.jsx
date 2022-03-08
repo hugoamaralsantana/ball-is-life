2510562f4f621aee52b25306e8729174d9a0295b
Victor Santana <victorhugoamaralsantana@gmail.com>
2022-03-08 13:45:47 -0500

    Search Revert

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,51 +1,45 @@
 import SearchBar from './SearchBar';
 import AutoComplete from './AutoComplete';
+import '../styles/InterfaceTheme.scss';
 import { useQuery } from 'react-query';
 import { useState } from 'react';
 
 function Header() {
-	const [playerQuery, updatePlayerQuery] = useState('');
-	const [acPlayers, setACPlayers] = useState([]);
+  const [playerQuery, updatePlayerQuery] = useState('');
 
-	const playerSearchQuery = useQuery('playerSearch', () =>
-		fetch(
-			'https://www.balldontlie.io/api/v1/players?page=0&per_page=100&search=' +
-				playerQuery
-		).then((res) => res.json())
-	);
+  const playerSearchQuery = useQuery('playerSearch', () =>
+    fetch(
+      'https://www.balldontlie.io/api/v1/players?search=' + playerQuery
+    ).then((res) => res.json())
+  );
 
-	const queriedPlayers = playerSearchQuery.data?.data;
+  const queriedPlayers = playerSearchQuery.data?.data;
 
-	if (queriedPlayers !== null) {
-		//setACPlayers(queriedPlayers);
-	}
-
-	return (
-		<div className='container mb-4 ml-0'>
-			<div className='row'>
-				<div className='col'>
-					<h1 className='display-1 m-0'>NBA TEAMS.</h1>
-				</div>
-				<div className='col d-flex align-items-end justify-content-end px-0 pb-3'>
-					<SearchBar
-						updatePlayerQuery={updatePlayerQuery}
-						playerQuery={playerQuery}
-					/>
-					<>
-						{acPlayers === [] ? (
-							<div styles='display:none' />
-						) : (
-							<AutoComplete
-								updatePlayerQuery={updatePlayerQuery}
-								autoCompleteList={acPlayers.slice(0, 10)}
-								playerQuery={playerQuery}
-							/>
-						)}
-					</>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className='container mb-4 ml-0'>
+      <div className='row'>
+        <div className='col'>
+          <h1 className='display-1 m-0'>NBA TEAMS.</h1>
+        </div>
+        <div className='col d-flex align-items-end justify-content-end px-0 pb-3 holder'>
+          <SearchBar
+            updatePlayerQuery={updatePlayerQuery}
+            playerQuery={playerQuery}
+          />
+          <>
+            {playerQuery === '' ? (
+              <div styles='display:none' />
+            ) : (
+              <AutoComplete
+                updatePlayerQuery={updatePlayerQuery}
+                autoCompleteList={queriedPlayers.slice(0, 10)}
+              />
+            )}
+          </>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
