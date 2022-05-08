b309528359a15fe37a2ee453bad32d411dd505a5
Victor Santana <victorsantana@1x-nat-vl930-172-30-70-254.wireless.umass.edu>
2022-05-08 15:34:40 -0400

    Testing Implementation

--- src/tests/app.test.jsx
+++ src/tests/app.test.jsx
@@ -1,6 +1,6 @@
 // __tests__/fetch.test.js
 import React from 'react'
-import {render, fireEvent, screen, waitFor} from '@testing-library/react'
+import {render, fireEvent, screen} from '@testing-library/react'
 import '@testing-library/jest-dom'
 
 import App from '../App'
